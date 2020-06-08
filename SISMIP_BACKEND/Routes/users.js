4b2a23b8537e77eb5dcf343b02f0e0189fe00d67
João Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2020-06-08 03:00:27 -0300

    Retirando arquivo de configuração n utilizado

--- SISMIP_BACKEND/Routes/users.js
+++ SISMIP_BACKEND/Routes/users.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 const User = require('../model/users.js');
 const bcrypt = require('bcrypt-nodejs');
@@ -8,93 +8,93 @@ const config = require('../config/config.js');
 
 //FUNÇÕES AUXILIARES
 
-var createUserToken = (Userid) =>{
-    return jwt.sign({id: Userid}, config.jwt_pass, {expiresIn: config.jwt_expires_in});
+var createUserToken = (Userid) => {
+    return jwt.sign({ id: Userid }, config.jwt_pass, { expiresIn: config.jwt_expires_in });
 }
 
 //FUNÇÕES PRICIPAIS
 
-router.get('/', auth, async (req,res) => {
-    
-    try{       
-        
+router.get('/', auth, async (req, res) => {
+
+    try {
+
         var user = await User.find({});
-        
-        if (user.length == 0) return res.send ({message: 'Nenhum Usuário Cadastrado'});
+
+        if (user.length == 0) return res.send({ message: 'Nenhum Usuário Cadastrado' });
 
         return res.send(user);
     }
-    
-    catch (err) {
-        return res.status(500).send ({error: 'Erro na Consulta de Usuário'});
 
-    } 
+    catch (err) {
+        return res.status(500).send({ error: 'Erro na Consulta de Usuário' });
+
+    }
 });
 
-router.get('/username', auth, async (req,res) => {
-    
-    const {username, password} = req.body;
+router.get('/username', auth, async (req, res) => {
 
-    try{       
-        
-        var user = await User.findOne({username});
-        
+    const { username, password } = req.body;
+
+    try {
+
+        var user = await User.findOne({ username });
+
         return res.send(user);
     }
-    
-    catch (err) {
-        return res.status(500).send ({error: 'Erro na Consulta de Usuário'});
 
-    } 
+    catch (err) {
+        return res.status(500).send({ error: 'Erro na Consulta de Usuário' });
+
+    }
 });
 
 
-router.get('/auth', async (req,res) => {
-    
-    const {username, password} = req.body;
+router.get('/auth', async (req, res) => {
 
-    if (!username || !password) return res.status(400).send ({error: 'Dados inseridos invalidos e/ou insuficientes'});
-    
-    try{       
-        
-        var user = await User.findOne({username}).select('+password');
-        if (!user) return res.status(400).send ({error: 'Usuário não cadastrado'});
+    const { username, password } = req.body;
 
-        var pass = bcrypt.compareSync(password, user.password);     
-        
-        if (!pass) return res.status(401).send ({error: 'Senha não confere'});
+    if (!username || !password) return res.status(400).send({ error: 'Dados inseridos invalidos e/ou insuficientes' });
+
+    try {
+
+        var user = await User.findOne({ username }).select('+password');
+        if (!user) return res.status(400).send({ error: 'Usuário não cadastrado' });
+
+        var pass = bcrypt.compareSync(password, user.password);
+
+        if (!pass) return res.status(401).send({ error: 'Senha não confere' });
 
         user.password = undefined;
-        return res.status(202).send({user, token: createUserToken(user.id)});
+        return res.status(202).send({ user, token: createUserToken(user.id) });
     }
-    
-    catch (err) {
-        return res.status(500).send ({error: 'Erro na Consulta de Usuário'});
 
-    } 
+    catch (err) {
+        return res.status(500).send({ error: 'Erro na Consulta de Usuário' });
+
+    }
 });
 
-router.post('/create', auth, async (req,res) => {
+router.post('/create', auth, async (req, res) => {
 
-    const {username, name, password} = req.body;
+    const { username, name, password } = req.body;
 
-    if (!username || !name || !password) return res.status(400).send ({error: 'Dados inseridos invalidos e/ou insuficientes'});
+    if (!username || !name || !password) return res.status(400).send({ error: 'Dados inseridos invalidos e/ou insuficientes' });
 
-    try{
-        
-        if (await User.findOne({username})) return res.status(400).send ({error: 'Usuário já cadastrado'});
-        
+    try {
+
+        if (await User.findOne({ username })) return res.status(400).send({ error: 'Usuário já cadastrado' });
+
         var user = await User.create(req.body);
 
         user.password = undefined;
         return res.status(201).send(user);
-       
+
     }
     catch (err) {
-        
-        if (err) return res.status(500).send ({error: 'Erro no Cadastro do Usuário'});
+
+        if (err) return res.status(500).send({ error: 'Erro no Cadastro do Usuário' });
     }
-    
+
 });
 
 
