c0a186c18d5ac4f5852e02e2199c29da793e692c
João Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2020-06-07 21:27:07 -0300

    Ajuste script de iniciação

--- SISMIP_BACKEND/app.js
+++ SISMIP_BACKEND/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const app = express();
+const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config/config.js');
