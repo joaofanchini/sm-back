fe982894c002ba74626a2437649ed3e06f3d5c6e
João Fanchini <joaofanchini@gmail.com>
2020-06-21 11:52:00 -0300

    Definindo porta pelo processo

--- SISMIP_BACKEND/app.js
+++ SISMIP_BACKEND/app.js
@@ -1,8 +1,8 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require("cors");
+const cors = require('cors');
 const config = require('./config/config.js');
 
 //CONEXÃO COM MONDO DB
@@ -10,25 +10,29 @@ const config = require('./config/config.js');
 const url_mdb = config.bd_string;
 //const options_mdb = {reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true,  useUnifiedTopology: true };
 
-const options_mdb = {poolSize: 5, useNewUrlParser: true,  useUnifiedTopology: true };
+const options_mdb = {
+  poolSize: 5,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
-mongoose.connect(url_mdb, options_mdb,);
+mongoose.connect(url_mdb, options_mdb);
 mongoose.set('useCreateIndex', true);
 
-mongoose.connection.on('error', (err) => {
-    console.log('Erro na conexão com o Banco de Dados' + err);
+mongoose.connection.on('error', err => {
+  console.log('Erro na conexão com o Banco de Dados' + err);
 });
 
 mongoose.connection.on('disconnected', () => {
-    console.log('Aplicação desconectada do Banco de Dados');
+  console.log('Aplicação desconectada do Banco de Dados');
 });
 
 mongoose.connection.on('connected', () => {
-    console.log('Aplicação conectada ao Banco de Dados');
+  console.log('Aplicação conectada ao Banco de Dados');
 });
 
 // BODY PARSER
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //CORS
@@ -41,6 +45,6 @@ const UsersRoute = require('./Routes/users.js');
 app.use('/', IndexRoute);
 app.use('/users', UsersRoute);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
