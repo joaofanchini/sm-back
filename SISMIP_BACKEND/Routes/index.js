1cb45fe93a2a633377c6b3b8af85a857d9cc92dd
João Fanchini <joaofanchini@gmail.com>
2020-06-21 15:06:32 -0300

    Ajuste retirando imports não utilizados

--- SISMIP_BACKEND/Routes/index.js
+++ SISMIP_BACKEND/Routes/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth.js');
-const config = require('../config/config.js');
 
 router.get('/', function(req, res) {
   return res.json({ message: 'Up service SISMIP' });
