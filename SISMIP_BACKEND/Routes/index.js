eb0bde68e6a95390ea2e2b9d91602b65cd4d21a0
João Fanchini <joaofanchini@gmail.com>
2020-06-21 12:20:18 -0300

    Ajuste para endpoint index

--- SISMIP_BACKEND/Routes/index.js
+++ SISMIP_BACKEND/Routes/index.js
@@ -1,19 +1,10 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth.js');
 const config = require('../config/config.js');
 
-router.get('/', auth, function (req,res){
-
-    return res.send({message: 'tudo ok com o get da raiz'});
-
+router.get('/', function(req, res) {
+  return res.json({ message: 'Up service SISMIP' });
 });
-
-router.post('/', auth, function (req,res){
-
-    return res.send({message: 'tudo ok com o post da raiz'});
-
-});
-
 
 module.exports = router;
