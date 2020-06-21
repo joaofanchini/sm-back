33013f7d53395d16b394733d47bb7af03e5966dc
vini-nascsilva <47404247+vini-nascsilva@users.noreply.github.com>
2020-06-21 17:47:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require ('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const config = require('./config/config.js');
+
+//CONEXÃO COM MONDO DB
+
+const url_mdb = config.bd_string;
+//const options_mdb = {reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true,  useUnifiedTopology: true };
+
+const options_mdb = {poolSize: 5, useNewUrlParser: true,  useUnifiedTopology: true };
+
+mongoose.connect(url_mdb, options_mdb,);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err) => {
+    console.log('Erro na conexão com o Banco de Dados' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Aplicação desconectada do Banco de Dados');
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Aplicação conectada ao Banco de Dados');
+});
+
+// BODY PARSER
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//CORS
+app.use(cors());
+
+//ROTAS
+const IndexRoute = require('./Routes/index.js');
+const UsersRoute = require('./Routes/users.js');
+const BugsRoute = require('./Routes/bugs.js');
+const PesticidesRoute = require('./Routes/pesticides.js');
+
+app.use('/', IndexRoute);
+app.use('/users', UsersRoute);
+app.use('/bugs', BugsRoute);
+app.use('/pesticides', PesticidesRoute);
+
+app.listen(3000);
+
+module.exports = app;
---
+++ model/users.js
@@ -0,0 +1,30 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const config = require('../config/config.js');
+
+const UserSchema = new Schema({
+      email: {type: String, required: true, unique: true, lowercase: true},
+      name: {type: String, required: true},
+      password: {type: String, required: true, select: false},
+      datecreated: {type: Date, default: Date.now}
+
+});
+
+UserSchema.pre('save', function (next){
+      
+      const user = this;
+
+      if(!user.isModified('password')) return next();
+
+      let salt = bcrypt.genSaltSync(config.bcrypt_saltRounds);
+  
+      let hash = bcrypt.hashSync(user.password, salt);
+
+      user.password = hash;
+
+      next();
+
+});
+
+module.exports = mongoose.model('Users', UserSchema);
