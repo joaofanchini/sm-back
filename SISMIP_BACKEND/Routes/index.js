375cb9de8cbbde6a618d5b78335c0290c06fdd56
João Fanchini <joaofanchini@gmail.com>
2020-06-21 15:00:30 -0300

    Add userData from auth Token

--- SISMIP_BACKEND/Routes/index.js
+++ SISMIP_BACKEND/Routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth.js');
 const config = require('../config/config.js');
 
-router.get('/', function(req, res) {
+router.get('/', auth, function(req, res) {
   return res.json({ message: 'Up service SISMIP' });
 });
 
--- SISMIP_BACKEND/middleware/auth.js
+++ SISMIP_BACKEND/middleware/auth.js
@@ -1,23 +1,20 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/config.js');
 
-var auth = (req,res, next) =>{
-    
-    var token_header = req.headers.authorization;
-    
-    if (!token_header) return res.status(401).send ({error: 'Token não Enviado'});
-    
-    token_header = token_header.replace('Bearer ','');
+var auth = (req, res, next) => {
+  var token_header = req.headers.authorization;
 
-    jwt.verify(token_header, config.jwt_pass, (err, decoded)=>{
-        
-        if (err) return res.status(401).send ({error: 'Token Inválido'});
-        
-        res.locals.auth_data = decoded;
+  if (!token_header)
+    return res.status(401).send({ error: 'Token não Enviado' });
 
-        return next();
-    });
-    
-}
+  token_header = token_header.replace('Bearer ', '');
+
+  jwt.verify(token_header, config.jwt_pass, (err, decoded) => {
+    if (err) return res.status(401).send({ error: 'Token Inválido' });
+
+    req.auth_data = { userId: decoded.id };
+    return next();
+  });
+};
 
 module.exports = auth;
