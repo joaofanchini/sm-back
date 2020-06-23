c6bb8bad48c5ec070242282a9f0b37a1915b8fa1
João Fanchini <joaofanchini@gmail.com>
2020-06-22 23:05:32 -0300

    Endpoint base plantation

--- SISMIP_BACKEND/Routes/plantations.js
+++ SISMIP_BACKEND/Routes/plantations.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth.js');
+
+//FUNÇÕES PRINCIPAIS
+
+router.get('/', auth, async (req, res) => {});
