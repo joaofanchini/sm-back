feaf7896acc60ea6aad7cfa90e027b35eb6e14a8
Thales Neves <thales.neves@prs.com.br>
2020-06-27 23:20:00 -0300

    add metodos para buscar por id em pragas e plantacoes

--- SISMIP_BACKEND/Routes/plagues.js
+++ SISMIP_BACKEND/Routes/plagues.js
@@ -6,143 +6,154 @@ const Plagues = require('../model/plagues.js');
 
 //FUNÇÕES PRICIPAIS
 
-router.get('/', auth, async (req, res) => {
-  try {
-    var bug = await Plagues.find({ user_id: req.auth_data.userId });
+router.get('/', auth, async(req, res) => {
+    try {
+        var bug = await Plagues.find({ user_id: req.auth_data.userId });
 
-    if (bug.length == 0)
-      return res.status(404).json({ message: 'Nenhum Inseto Cadastrado' });
+        if (bug.length == 0)
+            return res.status(404).json({ message: 'Nenhum Inseto Cadastrado' });
 
-    return res.json(bug);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Erro na Consulta de Insetos' });
-  }
+        return res.json(bug);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Erro na Consulta de Insetos' });
+    }
 });
 
-router.get('/plague/:name', auth, async (req, res) => {
-  let name = req.params.name;
+router.get('/plague/:name', auth, async(req, res) => {
+    let name = req.params.name;
 
-  try {
-    var bug = await Plagues.findOne({ user_id: req.auth_data.userId, name });
+    try {
+        var bug = await Plagues.findOne({ user_id: req.auth_data.userId, name });
 
-    if (!bug)
-      return res.status(404).json({ message: 'Nenhum Inseto Encontrado' });
+        if (!bug)
+            return res.status(404).json({ message: 'Nenhum Inseto Encontrado' });
 
-    return res.json(bug);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Erro na Consulta de Insetos' });
-  }
+        return res.json(bug);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Erro na Consulta de Insetos' });
+    }
 });
 
-router.post('/create', auth, async (req, res) => {
-  const {
-    name,
-    description,
-    na_phase_r,
-    na_phase_v,
-    initial_phase,
-    end_phase
-  } = req.body;
+router.get('/:id', auth, async(req, res) => {
+    let id = req.params.id;
 
-  if (
-    !name ||
-    !description ||
-    !na_phase_r ||
-    !na_phase_v ||
-    !initial_phase ||
-    !end_phase
-  ) {
-    return res
-      .status(400)
-      .json({ error: 'Dados inseridos invalidos e/ou insuficientes' });
-  }
-  try {
-    if (await Plagues.findOne({ user_id: req.auth_data.userId, name }))
-      return res.status(400).json({ error: 'Inseto já cadastrado' });
+    try {
+        var bug = await Plagues.findOne({ user_id: req.auth_data.userId, _id: id });
 
-    let bugDto = {
-      user_id: req.auth_data.userId,
-      name,
-      description,
-      na_phase_r,
-      na_phase_v,
-      initial_phase,
-      end_phase
-    };
+        if (!bug)
+            return res.status(404).json({ message: 'Nenhum Inseto Encontrado' });
 
-    var bug = await Plagues.create(bugDto);
-
-    return res.status(201).json(bug);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Erro no Cadastro de Inseto' });
-  }
-});
-router.post('/delete', auth, async (req, res) => {
-  const { name } = req.body;
-
-  try {
-    var bug = await Plagues.deleteOne({ user_id: req.auth_data.userId, name });
-
-    if (bug.deletedCount == 0)
-      return res.status(410).json({ message: 'Inseto Inexistente' });
-
-    return res.status(200).json({ message: 'Inseto Excluído' });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Erro ao Excluir Inseto' });
-  }
+        return res.json(bug);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Erro na Consulta de Insetos' });
+    }
 });
 
-router.post('/update', auth, async (req, res) => {
-  const {
-    name,
-    description,
-    na_phase_r,
-    na_phase_v,
-    initial_phase,
-    end_phase,
-    image
-  } = req.body;
+router.post('/create', auth, async(req, res) => {
+    const {
+        name,
+        description,
+        na_phase_r,
+        na_phase_v,
+        initial_phase,
+        end_phase
+    } = req.body;
 
-  if (
-    !name ||
-    !description ||
-    !na_phase_r ||
-    !na_phase_v ||
-    !initial_phase ||
-    !end_phase
-  ) {
-    return res
-      .status(400)
-      .json({ error: 'Dados inseridos invalidos e/ou insuficientes' });
-  }
+    if (!name ||
+        !description ||
+        !na_phase_r ||
+        !na_phase_v ||
+        !initial_phase ||
+        !end_phase
+    ) {
+        return res
+            .status(400)
+            .json({ error: 'Dados inseridos invalidos e/ou insuficientes' });
+    }
+    try {
+        if (await Plagues.findOne({ user_id: req.auth_data.userId, name }))
+            return res.status(400).json({ error: 'Inseto já cadastrado' });
 
-  try {
-    var bug = await Plagues.updateOne(
-      { user_id: req.auth_data.userId, name },
-      {
-        $set: {
-          description: description,
-          na_phase_r: na_phase_r,
-          na_phase_v: na_phase_v,
-          initial_phase: initial_phase,
-          end_phase: end_phase,
-          image: image
-        }
-      }
-    );
+        let bugDto = {
+            user_id: req.auth_data.userId,
+            name,
+            description,
+            na_phase_r,
+            na_phase_v,
+            initial_phase,
+            end_phase
+        };
 
-    if (bug.n == 0)
-      return res.status(410).json({ message: 'Inseto Inexistente' });
+        var bug = await Plagues.create(bugDto);
 
-    return res.status(200).json({ message: 'Inseto Alterado' });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Erro ao Alterar Inseto' });
-  }
+        return res.status(201).json(bug);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Erro no Cadastro de Inseto' });
+    }
+});
+router.post('/delete', auth, async(req, res) => {
+    const { name } = req.body;
+
+    try {
+        var bug = await Plagues.deleteOne({ user_id: req.auth_data.userId, name });
+
+        if (bug.deletedCount == 0)
+            return res.status(410).json({ message: 'Inseto Inexistente' });
+
+        return res.status(200).json({ message: 'Inseto Excluído' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Erro ao Excluir Inseto' });
+    }
+});
+
+router.post('/update', auth, async(req, res) => {
+    const {
+        name,
+        description,
+        na_phase_r,
+        na_phase_v,
+        initial_phase,
+        end_phase,
+        image
+    } = req.body;
+
+    if (!name ||
+        !description ||
+        !na_phase_r ||
+        !na_phase_v ||
+        !initial_phase ||
+        !end_phase
+    ) {
+        return res
+            .status(400)
+            .json({ error: 'Dados inseridos invalidos e/ou insuficientes' });
+    }
+
+    try {
+        var bug = await Plagues.updateOne({ user_id: req.auth_data.userId, name }, {
+            $set: {
+                description: description,
+                na_phase_r: na_phase_r,
+                na_phase_v: na_phase_v,
+                initial_phase: initial_phase,
+                end_phase: end_phase,
+                image: image
+            }
+        });
+
+        if (bug.n == 0)
+            return res.status(410).json({ message: 'Inseto Inexistente' });
+
+        return res.status(200).json({ message: 'Inseto Alterado' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Erro ao Alterar Inseto' });
+    }
 });
 
 module.exports = router;
